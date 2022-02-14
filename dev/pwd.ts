00a090eca0a42842d2e1669b070767cd03754f4b
Aleoshenka <alexnegroylupitaaa@gmail.com>
2022-02-14 15:01:35 -0600

    Initialize wes/git-recurse-submodules

---
+++ dev/pwd.ts
@@ -0,0 +1,18 @@
+const completionSpec: Fig.Spec = {
+  name: "pwd",
+  description: "Print name of current/working directory",
+  options: [
+    {
+      name: ["-L", "--logical"],
+      description: "Use PWD from environment, even if it contains symlinks",
+    },
+    { name: ["-P", "--physical"], description: "Avoid all symlinks" },
+    { name: "--help", description: "Display this help and exit" },
+    {
+      name: "--version",
+      description: "Output version information and exit",
+    },
+  ],
+};
+
+export default completionSpec;
