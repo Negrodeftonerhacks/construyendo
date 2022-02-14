7994f1359aba04a84917f227a9bd5885a37e73ad
Aleoshenka <alexnegroylupitaaa@gmail.com>
2022-02-14 14:59:51 -0600

    Initialize feat/add_linting

---
+++ dev/cat.ts
@@ -0,0 +1,8 @@
+export const catCompletionSpec: Fig.Spec = {
+    name: 'cat',
+    description: 'concatenate and print files',
+    args: {
+        variadic: true,
+        template: 'filepaths',
+    },
+};
