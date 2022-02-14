963157b46c255d8394e639da4f434bcd2ca23bf3
Aleoshenka <alexnegroylupitaaa@gmail.com>
2022-02-14 15:00:01 -0600

    Initialize feat/add_heroku_spec

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+    "plugin:fig-linter/recommended",
+  ],
+  plugins: ["fig-linter"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        // Optional Prettier config changes
+        trailingComma: "es5",
+        printWidth: 80,
+      },
+    ],
+    "no-unused-vars": ["off"],
+    "no-var": ["off"],
+    "@typescript-eslint/no-unused-vars": ["off"],
+  },
+};
