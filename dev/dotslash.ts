3c3c879eb926e06d63e68956c1faaaaadace010b
Aleoshenka <alexnegroylupitaaa@gmail.com>
2022-02-14 15:00:19 -0600

    Initialize tom/tmux

---
+++ dev/dotslash.ts
@@ -0,0 +1,8 @@
+// use to complete on files when they are the first string in a branch
+
+export const completionSpec: Fig.Spec = {
+  name: "dotslash",
+  args: {
+    template: "filepaths",
+  },
+};
