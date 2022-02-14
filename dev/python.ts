cf6622e4ca23f8b593b45b0597e13a90410e74f9
Aleoshenka <alexnegroylupitaaa@gmail.com>
2022-02-14 15:00:02 -0600

    Initialize mschrage/git-add-folders

---
+++ dev/python.ts
@@ -0,0 +1,30 @@
+export const completionSpec: Fig.Spec = {
+  name: "python3",
+  description: "Run the python interpretor",
+  args: {
+    name: "python script",
+    isScript: true,
+    generators: {
+      template: "filepaths",
+      filterTemplateSuggestions: function (paths) {
+        return paths.filter((file) => {
+          if (typeof file.name === "string") {
+            return file.name.endsWith(".py") || file.name.endsWith("/");
+          }
+          return false;
+        });
+      },
+    },
+  },
+  options: [
+    {
+      name: ["-c"],
+      insertValue: "-c '{cursor}'",
+      description: "program passed in as string (terminates option list)",
+      args: {
+        name: "command",
+        isCommand: true,
+      },
+    },
+  ],
+};
